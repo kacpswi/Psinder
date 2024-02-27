4d845e6def446af573c1fb6c2a33d1cbf5035c1c
Kacper Świątek <kacperswiatek99@gmail.com>
2024-02-27 14:53:05 +0100

    Added CORS policy

--- Program.cs
+++ Program.cs
@@ -34,6 +34,8 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
